5a7596eda7304aa2df735f654bea18f0a972c786
DuMbl4 <DuMbl4_63@mail.ru>
2019-01-12 13:38:01 +0400

    Lesson1

---
+++ Task1.py
@@ -0,0 +1,31 @@
+#слова в строковом формате
+word1 = 'разработка'
+word2 = 'сокет'
+word3 = 'декоратор'
+
+#проверяем тип переменных
+print(type(word1))
+print(type(word2))
+print(type(word3))
+
+#проверяем содержимое переменных
+print(word1)
+print(word2)
+print(word3)
+
+print('///////////////////////////////////////////')
+
+#слова в Unicode формате
+word1_u = '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430'
+word2_u = '\u0441\u043e\u043a\u0435\u0442'
+word3_u = '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'
+
+#проверяем тип переменных
+print(type(word1_u))
+print(type(word2_u))
+print(type(word3_u))
+
+#проверяем содержимое переменных
+print(word1_u)
+print(word2_u)
+print(word3_u)
