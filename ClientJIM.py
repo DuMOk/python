bc7762589419bd0cf06da1a206c1430ef3a0b1e2
DuMbl4 <DuMbl4_63@mail.ru>
2019-01-15 22:55:39 +0400

    Lesson6

--- ClientJIM.py
+++ ClientJIM.py
@@ -1,13 +1,29 @@
 from socket import *
 import sys
 import json
-import time
-import logging
+import time, datetime
+import logging, inspect
 import log.client_log_config
+from functools import wraps
 
-def main(): 
-  logger = logging.getLogger('client')
+logger = logging.getLogger('client')
+
+def decorator(func):
+  @wraps(func)
+  def decorated(*args, **kwargs):
+    logger.debug(f"Function name is {func.__name__} called with args {args} and named args {kwargs}")
+    logger.debug(f"<{datetime.datetime.now()}> Function {func.__name__} call of function {inspect.stack()[1].function}")
+    return func(*args, **kwargs)
   
+  return decorated
+  
+
+@decorator
+def recv_json(bytestr, encoding: str = "utf-8"):
+  return json.loads(bytestr.decode(encoding))
+
+
+def main():   
   logger.info('Start main client function!')
   client = socket(AF_INET, SOCK_STREAM)
   
@@ -44,7 +60,7 @@ def main():
   
   client.send(json.dumps(msg).encode('utf-8'))
   data = client.recv(1000000)
-  msg = json.loads(data.decode('utf-8'))
+  msg = recv_json(data, 'utf-8')
   
   if (msg.get('response') == 200):
     print('Сообщение от сервера: ', msg.get('alert'))
--- log/client_log_config.py
+++ log/client_log_config.py
@@ -1,3 +1,17 @@
+# Для проекта «Мессенджер» реализовать логирование с использованием модуля logging:
+# В директории проекта создать каталог log, в котором для клиентской и серверной сторон в отдельных модулях 
+# формата client_log_config.py и server_log_config.py создать логгеры;
+# В каждом модуле выполнить настройку соответствующего логгера по следующему алгоритму:
+# - Создание именованного логгера;
+# - Сообщения лога должны иметь следующий формат: "<дата-время> <уровень_важности> <имя_модуля> <сообщение>";
+# - Журналирование должно производиться в лог-файл;
+# - На стороне сервера необходимо настроить ежедневную ротацию лог-файлов.    
+# - Реализовать применение созданных логгеров для решения двух задач:
+# - Журналирование обработки исключений try/except. Вместо функции print() использовать журналирование 
+#   и обеспечить вывод служебных сообщений в лог-файл;
+# - Журналирование функций, исполняемых на серверной и клиентской сторонах при работе мессенджера
+
+
 import logging
 import sys      
 
@@ -6,11 +20,11 @@ logger = logging.getLogger('client')
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s - %(message)s ")
 
 fh = logging.FileHandler("log/client.log", encoding='utf-8')
-fh.setLevel(logging.INFO)
+fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 
 logger.addHandler(fh)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 console = logging.StreamHandler(sys.stdout)
 console.setLevel(logging.INFO)
