93a9156eb7e9a9fcc21a1d6a72db17859060c2b0
DuMbl4 <DuMbl4_63@mail.ru>
2019-01-15 23:04:21 +0400

    Lesson3

--- ClientJIM.py
+++ ClientJIM.py
@@ -1,76 +1,53 @@
 from socket import *
 import sys
 import json
-import time, datetime
-import logging, inspect
-import log.client_log_config
-from functools import wraps
+import time
 
-logger = logging.getLogger('client')
+client = socket(AF_INET, SOCK_STREAM)
 
-def decorator(func):
-  @wraps(func)
-  def decorated(*args, **kwargs):
-    logger.debug(f"Function name is {func.__name__} called with args {args} and named args {kwargs}")
-    logger.debug(f"<{datetime.datetime.now()}> Function {func.__name__} call of function {inspect.stack()[1].function}")
-    return func(*args, **kwargs)
-  
-  return decorated
-  
-
-@decorator
-def recv_json(bytestr, encoding: str = "utf-8"):
-  return json.loads(bytestr.decode(encoding))
-
-
-def main():   
-  logger.info('Start main client function!')
-  client = socket(AF_INET, SOCK_STREAM)
-  
-  if (len(sys.argv) == 1):
-    logger.fatal('Не указан IP адрес для подключения к серверу!')
+if (len(sys.argv) == 1):
+  print('Не указан IP адрес для подключения к серверу!')
+  sys.exit()
+elif (len(sys.argv) == 2):
+  try: 
+    client.connect((sys.argv[1], 7777))
+  except ConnectionRefusedError:
+    print(f'Не удалось подключиться к серверу {sys.argv[1]} по TCP порту 7777')
+    sys.exit()  
+elif (len(sys.argv) == 3):
+  try:
+    client.connect((sys.argv[1], int(sys.argv[2])))
+  except ConnectionRefusedError:
+    print(f'Не удалось подключиться к серверу {sys.argv[1]} по TCP порту {sys.argv[2]}')
     sys.exit()
-  elif (len(sys.argv) == 2):
-    try: 
-      client.connect((sys.argv[1], 7777))
-    except ConnectionRefusedError:
-      logger.error(f'Не удалось подключиться к серверу {sys.argv[1]} по TCP порту 7777')
-      sys.exit()  
-  elif (len(sys.argv) == 3):
-    try:
-      client.connect((sys.argv[1], int(sys.argv[2])))
-    except ConnectionRefusedError:
-      logger.error(f'Не удалось подключиться к серверу {sys.argv[1]} по TCP порту {sys.argv[2]}')
-      sys.exit()
-  else:
-    print('Слишком много параметров!')
-    sys.exit()
-          
-  timestamp = int(time.time())
-  
-  msg = {
-    "action": "presence",
-    "time": timestamp,
-    "type": "status",
-    "user": {
-      "account_name": "DuMOk",
-      "status": "online"
-    }
+else:
+  print('Слишком много параметров!')
+  sys.exit()
+        
+timestamp = int(time.time())
+
+msg = {
+  "action": "presence",
+  "time": timestamp,
+  "type": "status",
+  "user": {
+    "account_name": "DuMOk",
+    "status": "online"
   }
-  
-  client.send(json.dumps(msg).encode('utf-8'))
-  data = client.recv(1000000)
-  msg = recv_json(data, 'utf-8')
-  
-  if (msg.get('response') == 200):
-    print('Сообщение от сервера: ', msg.get('alert'))
-  elif (msg.get('response') == 400):
-    print('Сообщение от сервера: ', msg.get('alert'))
-  
-  client.close()
-  logger.info('Stop main client function!')
+}
 
-if __name__ == '__main__':
-  main()
+client.send(json.dumps(msg).encode('utf-8'))
+data = client.recv(1000000)
+msg = json.loads(data.decode('utf-8'))
+
+if (msg.get('response') == 200):
+  print('Сообщение от сервера: ', msg.get('alert'))
+elif (msg.get('response') == 400):
+  print('Сообщение от сервера: ', msg.get('alert'))
+
+client.close()
+
+
+
 
 
--- ServerJIM.py
+++ ServerJIM.py
@@ -3,68 +3,58 @@ import time
 from datetime import datetime
 import argparse
 import json
-import logging
-import log.server_log_config
 
-def main():
-  logger = logging.getLogger('server')
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', action='store', help='TCP-порт для работы (по умолчанию 7777)')
+parser.add_argument('-a', action='store', help='IP адрес для прослушивания(по умолчанию слушает все доступные адреса)')
 
-  logger.debug('Start main server function!')
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-p', action='store', help='TCP-порт для работы (по умолчанию 7777)')
-  parser.add_argument('-a', action='store', help='IP адрес для прослушивания(по умолчанию слушает все доступные адреса)')
+port = 7777
+address = ''
+
+if (parser.parse_args().p):
+  port = int(parser.parse_args().p)
+if (parser.parse_args().a):
+  address = parser.parse_args().a
+
+server = socket(AF_INET, SOCK_STREAM)  
+server.bind((address, port))                
+server.listen(10)                       
+
+
+msgOK = {
+  "response": 200,
+  "time": 0,
+  "alert": ""
+}
+
+msgError = {
+  "response": 400,
+  "time": 0,
+  "alert": "Error in JSON request"
+}
+
+while True:
+  client, addr = server.accept()
+  data = client.recv(1000000)
+  msg = json.loads(data.decode('utf-8'))
   
-  port = 7777
-  address = ''
-  
-  if (parser.parse_args().p):
-    port = int(parser.parse_args().p)
-  if (parser.parse_args().a):
-    address = parser.parse_args().a
-  
-  server = socket(AF_INET, SOCK_STREAM)  
-  server.bind((address, port))                
-  server.listen(10)                       
-  
-  
-  msgOK = {
-    "response": 200,
-    "time": 0,
-    "alert": ""
-  }
-  
-  msgError = {
-    "response": 400,
-    "time": 0,
-    "alert": "Error in JSON request"
-  }
-  
-  while True:
-    client, addr = server.accept()
-    data = client.recv(1000000)
-    msg = json.loads(data.decode('utf-8'))
-    
-    if (msg.get('action') == 'presence'):
-      if (msg.get('user').get('status') == 'online'):
-        user_time = datetime.utcfromtimestamp(msg.get('time')).strftime('%Y-%m-%d %H:%M:%S')
-        msg_cl = f"User {msg.get('user').get('account_name')} is now {msg.get('user').get('status')}! Time: {user_time} "
-        print(msg_cl)
-        logger.info(msg_cl)
-        msgOK['alert'] = f"Welcome to world, {msg.get('user').get('account_name')}!"
-        msgOK['time'] = int(time.time())
-        msg = msgOK
-      else: 
-        msgError['time'] = int(time.time())
-        msg = msgError
+  if (msg.get('action') == 'presence'):
+    if (msg.get('user').get('status') == 'online'):
+      user_time = datetime.utcfromtimestamp(msg.get('time')).strftime('%Y-%m-%d %H:%M:%S')
+      print(f"User {msg.get('user').get('account_name')} is now {msg.get('user').get('status')}! Time: {user_time} ")
+      msgOK['alert'] = f"Welcome to world, {msg.get('user').get('account_name')}!"
+      msgOK['time'] = int(time.time())
+      msg = msgOK
     else: 
       msgError['time'] = int(time.time())
       msg = msgError
-     
-    client.send(json.dumps(msg).encode('utf-8'))
-    client.close()
+  else: 
+    msgError['time'] = int(time.time())
+    msg = msgError
+   
+  client.send(json.dumps(msg).encode('utf-8'))
+  client.close()
 
-if __name__ == '__main__':
-  main()
 
 
 
